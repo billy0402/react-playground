3c3cd756d000ab7d13026b8f0d72e3867d4a6be9
Billy Huang <10646003@ntub.edu.tw>
2022-09-12 18:06:07 +0800

    feat(home-page): add test content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,68 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
+import { useTextSelection } from 'use-text-selection';
 
 const HomePage: NextPage = () => {
-  return <h1>Hello, world!</h1>;
+  const { clientRect, isCollapsed } = useTextSelection();
+  const [currentRange, setCurrentRange] = useState<any>(null);
+  const [link, setLink] = useState('');
+
+  const createRangeUrl = () => {
+    let tagString = document.createElement('a');
+    tagString.href = link;
+
+    currentRange?.surroundContents(tagString);
+  };
+
+  const createRangeTag = (tagName: 'b') => {
+    let selection = window.getSelection();
+    const range = selection?.getRangeAt(0);
+
+    let tagString = document.createElement(tagName);
+    console.log(selection);
+    console.log(range);
+
+    tagString.classList.add('bold');
+    range?.surroundContents(tagString);
+  };
+
+  const createRange = () => {
+    let selection = window.getSelection();
+    const range = selection?.getRangeAt(0);
+    setCurrentRange(range);
+  };
+
+  return (
+    <>
+      <article contentEditable suppressContentEditableWarning>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore maiores
+        doloremque officiis, reiciendis asperiores distinctio. Veritatis
+        suscipit quis aspernatur blanditiis. Ipsa iusto officiis quae delectus.
+        Provident fugiat facilis porro dignissimos!
+      </article>
+      {!isCollapsed && clientRect && (
+        <div
+          style={{
+            left: clientRect.x,
+            top: clientRect.y + clientRect.height,
+            position: 'absolute',
+            display: 'block',
+          }}
+          id='control'
+        >
+          <button onClick={createRange}>新增連結區域</button>
+          <button onClick={() => createRangeTag('b')}>粗體文字</button>
+          <button onClick={createRange}>斜體文字</button>
+        </div>
+      )}
+
+      <span>
+        {currentRange?.startOffset}
+        <input type='text' onChange={(e) => setLink(e.target.value)} />
+      </span>
+      <span onClick={() => createRangeUrl()}>新增連結</span>
+    </>
+  );
 };
 
 export default HomePage;
